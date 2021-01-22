4f924033dab0f0ee0ac52022c9990c3e5344d455
edward <edhuosb18@gmail.com>
2021-01-22 15:54:10 -0500

    eliminando comentarios

--- public/js/alertas.js
+++ public/js/alertas.js
@@ -1,24 +1,16 @@
-//Alertas
 function Alertas(titulo, descripcion, tipoAlerta) {
     Swal.fire(titulo, descripcion, tipoAlerta);
 }
 
 function Personalizar() {
-    /*Alertas("ADVERTENCIA", "Primero registrar o loguear para pedir a personalizar un diseño.", "warning");*/
     Swal.fire({
         title: 'ADVERTENCIA',
         text: 'Registrar o ingresar, antes de personalizar un diseño.',
         showCancelButton: true,
         confirmButtonText: `Ir a Personalizar`,
-      }).then((result) => {
+    }).then((result) => {
         if (result.isConfirmed) {
             window.location = '../vistas/login.php';
-        } 
-      });
+        }
+    });
 }
-
-/*
-$("#personalizar").click(function(){
-    Personalizar();
-});*/
-
--- public/js/registrate.js
+++ public/js/registrate.js
@@ -6,16 +6,15 @@ let form2 = document.getElementById("form2")
 let form = document.getElementById("form1")
 
 identificate.addEventListener("click", (e) => {
-   e.preventDefault()
-    
+    e.preventDefault()
+
     form2.classList.remove("ocultar")
     form.classList.remove("aparecer")
 })
 
-registrate2.addEventListener("click",(e)=>{
-e.preventDefault()
-form.classList.add("aparecer")
-form2.classList.add("ocultar")
+registrate2.addEventListener("click", (e) => {
+    e.preventDefault()
+    form.classList.add("aparecer")
+    form2.classList.add("ocultar")
 
 })
-
